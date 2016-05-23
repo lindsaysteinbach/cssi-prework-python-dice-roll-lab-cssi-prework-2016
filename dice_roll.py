7fbd5f7fdd079dddb736405cf42b039a39989e50
lindsaysteinbach <lsteinbach@google.com>
2016-05-23 15:48:34 -0400

    Done.

--- dice_roll.py
+++ dice_roll.py
@@ -1,3 +1,8 @@
 # Create function `roll` that returns a random number between 1 and 6
 
 # Feel free to google "how to generate a random number in python"
+
+import random
+
+def dice_roll():
+  return random.randrange(1,6)
